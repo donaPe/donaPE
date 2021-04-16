7ce90fadbaafd0972e80510ce06c9b6febb5600e
JuanArce <arcalcjuan@gmail.com>
2021-04-16 09:05:07 -0500

    base proyecto

---
+++ src/test/java/com/desafio5/donape/DonapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio5.donape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
