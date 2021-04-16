8cbce2e2f14faa24d378a462fc4d57243ceba043
JuanArce <arcalcjuan@gmail.com>
2021-04-16 10:39:43 -0500

    creacion de endpoint ubicacion

--- src/main/java/com/desafio5/donape/controller/UbicacionController.java
+++ src/main/java/com/desafio5/donape/controller/UbicacionController.java
@@ -1,4 +1,24 @@
 package com.desafio5.donape.controller;
 
+import com.desafio5.donape.persistence.entities.Ubicacion;
+import com.desafio5.donape.service.IUbicacionService;
+import com.desafio5.donape.service.Impl.UbicacionServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class UbicacionController {
+    @Autowired
+    private IUbicacionService ubicacionService;
+
+    @GetMapping("/donape/ubicacion")
+    public ResponseEntity<List<Ubicacion>> findAll(){
+        var answerClient = ubicacionService.findAll();
+        return ResponseEntity.ok(answerClient);
+    }
+
 }
